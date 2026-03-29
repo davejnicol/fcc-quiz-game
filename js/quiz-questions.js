628a3dbc74153d81696f7537f78fdaeb463b847a
Dave J Nicol <46354316+davejnicol@users.noreply.github.com>
2026-03-29 13:54:38 -0400

    First 5 questions

---
+++ js/quiz-questions.js
@@ -0,0 +1,58 @@
+// This file contains all the questions used in for the quiz
+export const quizQuestions = [
+    {
+        id: 1,
+        catergory: "Geography",
+        question: "What is the capital of France?",
+        answers: [
+            { text: "London", correct: false },
+            { text: "Berlin", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Madrid", correct: false },
+        ],
+    },
+    {
+        id: 2,
+        catergory: "Astronomy",
+        question: "Which planet is known as the Red Planet?",
+        answers: [
+            { text: "Venus", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: false },
+        ],
+    },
+    {
+        id: 3,
+        catergory: "Geography",
+        question: "What is the largest ocean on Earth?",
+        answers: [
+            { text: "Atlantic Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Arctic Ocean", correct: false },
+            { text: "Pacific Ocean", correct: true },
+        ],
+    },
+    {
+        id: 4,
+        catergory: "Coding",
+        question: "Which of these is NOT a programming language?",
+        answers: [
+            { text: "Java", correct: false },
+            { text: "Python", correct: false },
+            { text: "Banana", correct: true },
+            { text: "JavaScript", correct: false },
+        ],
+    },
+    {
+        id: 5,
+        catergory: "Science",
+        question: "What is the chemical symbol for gold?",
+        answers: [
+            { text: "Go", correct: false },
+            { text: "Gd", correct: false },
+            { text: "Au", correct: true },
+            { text: "Ag", correct: false },
+        ],
+    },
+];
